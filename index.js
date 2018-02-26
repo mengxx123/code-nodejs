e7d395b5bdbaf04cb6374a803cb30b3373e371cf
gitcjh <1418503647@.qq.com>
2018-02-26 18:02:10 +0800

    backup

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ var contentText = fs.readFileSync('123.sql', 'utf-8')
 // console.log(contentText);
 
 let ret = contentText.match(/(CREATE TA[\w\W]+?;)/g)
-console.log(ret.length)
+// console.log(ret.length)
 for (let sql of ret) {
     // console.log(sql)
 }
 let result = generateCode(ret[0])
-console.log(result)
+// console.log(result)
--- sqlUtil2.js
+++ sqlUtil2.js
@@ -1,12 +1,34 @@
 function generateCode(sql) {
-    let lines = sql.split('\n')
-    for (let line of lines) {
+    let lines = sql.split(/\r|\n/)
+    let obj = {
+        notNull: false
+    }
+    for (let idx = 0; idx < lines.length; i++) {
+        let line = lines[i]
         line = line.replace(/(^\s*)|(\s*$)/g, '')
-        console.log(line)
-        if (line.test(/^`/)) {
-            
+        if (idx === 0) {
+            console.log('表格名' + line)
+        }
+
+        // line = line.replace(/(^\s*)|(\s*$)/g, '')
+        if (/^`/.test(line)) {
+            let arr = line.split(' ')
+            for (let i = 0; i < arr.length; i++) {
+                if (i === 0) {
+                    obj.columnName = arr[i].replace(/`/g, '')
+                } else if (i === 1) {
+                    obj.dataType = arr[i].replace(/`/g, '')
+                } else if (arr[i] === 'NOT') {
+                    obj.notNull = true
+                } else  if (arr[i] === 'DEFAULT') {
+                    obj.default = arr[i + 1]
+                } else  if (arr[i] === 'COMMENT') {
+                    obj.comment = arr[i + 1]
+                }
+            }
         }
     }
+    console.log(obj)
 }
 
 module.exports = generateCode
